9fea176e5033b4ac3d24ab14ab61c72ec934d28e
Rehan Manzoor <manzoorr@amazon.com>
2021-03-13 16:06:19 +0400

    Add spinner while searching

--- app/Http/Livewire/PublicApi.php
+++ app/Http/Livewire/PublicApi.php
@@ -12,11 +12,16 @@ class PublicApi extends Component
 
     public function updatedSearch()
     {
+        // if search term is less then 3 do nothing.
+        if (strlen($this->search) < 4) {
+            return;
+        }
+
         $response = Http::get("https://api.github.com/search/users?q={$this->search}");
 
         $responseJson = json_decode($response->body(), true);
 
-        $this->searchResults = $responseJson['items'];
+        $this->searchResults = isset($responseJson['items']) ? $responseJson['items'] : [];
     }
 
     public function render()
--- resources/views/livewire/public-api.blade.php
+++ resources/views/livewire/public-api.blade.php
@@ -1,24 +1,44 @@
 <div>
 
     <div class="form-group">
-        <label for="">Search</label>
-        <input type="text" class="form-control" wire:model.debounce.500ms="search">
+        <label for="">
+            Search
+        </label>
+        <input type="search" class="form-control" wire:model.debounce.300ms="search">
+    </div>
+
+    <div class="form-group mt-3" wire:loading>
+        <div class="spinner-border text-primary" role="status">
+        </div> Searching...
     </div>
 
 
-    <div class="row">
-        @foreach($searchResults as $searchResult)
-            <div class="col-3 mt-2">
-                <div class="card h-100">
-                    <img src="{{ $searchResult['avatar_url'] }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <a href="{{ $searchResult['html_url'] }}" class="card-title">
-                            <i class="bi bi-github"></i> {{ $searchResult['login'] }}
-                        </a>
+
+    @if(strlen($this->search) > 3)
+        <div class="row">
+            @forelse($searchResults as $searchResult)
+                <div class="col-3 mt-2">
+                    <div class="card h-100">
+                        <img src="{{ $searchResult['avatar_url'] }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <a href="{{ $searchResult['html_url'] }}" class="card-title">
+                                <i class="bi bi-github"></i> {{ $searchResult['login'] }}
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
-    </div>
+            @empty
+                <div class="jumbotron jumbotron-fluid">
+                    <div class="container">
+                        <h1 class="display-4">Sorry!</h1>
+                        <p class="lead">
+                            Your search term brought no results.
+                        </p>
+                    </div>
+                </div>
+
+            @endforelse
+        </div>
+    @endif
 
 </div>
